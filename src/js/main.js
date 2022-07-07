c3641d043566e4b9bd7200cd4b90b6c51fb9fc2f
Marty <Marty2009@yandex.ru>
2022-07-07 23:06:36 +0300

    Сделал секцию с контактами, кроме картинки когда js нет, разобрался с кастомным селектом

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+const selectDefaultParams = {
+  containerClass: 'custom-select-container',
+  openerClass: 'custom-select-opener',
+  panelClass: 'custom-select-panel',
+  optionClass: 'custom-select-option',
+  optgroupClass: 'custom-select-optgroup',
+  isSelectedClass: 'is-selected',
+  hasFocusClass: 'has-focus',
+  isDisabledClass: 'is-disabled',
+  isOpenClass: 'is-open'
+}
+const mySelects = customSelect(document.querySelector('.contacts__select'), selectDefaultParams);
+
+console.log(mySelects);
