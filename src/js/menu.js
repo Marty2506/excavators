af16fa623de1525694506a992eb2a724cad4c932
Marty <Marty2009@yandex.ru>
2022-07-12 19:26:40 +0300

    переписал js для меню, добавил класс для ссылки без дальнейшего меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,15 @@
 const menu = document.querySelector('.menu');
-const menuLinks = menu.querySelectorAll('.menu__main > .menu__list > .menu__item > .menu__link');
+const menuSubs = menu.querySelectorAll('.menu__sub');
 
-menuLinks.forEach(menuLink => {
-  menuLink.addEventListener('click', (evt) => {
-    const submenu = evt.target.parentNode.querySelector('.menu__sub');
-    submenu.classList.add('menu__sub--active');
-    const backButton = submenu.querySelector('.menu__back-button');
-    backButton.addEventListener('click', () => {
-      submenu.classList.remove('menu__sub--active');
-    })
-  })
+menuSubs.forEach(submenu => {
+  const link = submenu.parentNode.querySelector('.menu__link');
+  if (link) {
+    link.addEventListener('click', () => {
+      submenu.classList.add('menu__sub--active');
+      const backButton = submenu.querySelector('.menu__back-button');
+      backButton.addEventListener('click', () => {
+        submenu.classList.remove('menu__sub--active');
+      });
+    });
+  }
 });
