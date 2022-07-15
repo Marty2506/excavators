91642053a7ee63759db50ceac93371f379cb5368
Marty <Marty2009@yandex.ru>
2022-07-15 19:04:24 +0300

    сделал кастомный селект для свежих спецпредложений

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,21 @@ const selectDefaultParams = {
   hasFocusClass: 'has-focus',
   isDisabledClass: 'is-disabled',
   isOpenClass: 'is-open'
-}
-const mySelects = customSelect(document.querySelector('.contacts__select'), selectDefaultParams);
+};
+const contactsSelect = new customSelect(document.querySelector('.contacts__select'), selectDefaultParams);
+
+const newOffersSelectParams = {
+  containerClass: 'new-offers__select-container',
+  openerClass: 'new-offers__select-opener',
+  panelClass: 'new-offers__select-panel',
+  optionClass: 'new-offers__select-option',
+  optgroupClass: 'new-offers__select-optgroup',
+  isSelectedClass: 'is-selected',
+  hasFocusClass: 'has-focus',
+  isDisabledClass: 'is-disabled',
+  isOpenClass: 'is-open'
+};
+const newOffersSelect = new customSelect(document.querySelector('.new-offers__select'), newOffersSelectParams);
 
 //  Включение автоматического воспроизведения видео
 const videoLocation  = document.querySelector('.index-header__video');
