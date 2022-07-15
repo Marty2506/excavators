c331df93680dd6c426f90ce83969d8bb85f6625c
Marty <Marty2009@yandex.ru>
2022-07-15 17:46:11 +0300

    сделал мобильную версию tech-tabs

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,10 +1,14 @@
 const tabs = document.querySelectorAll('.tabs');
+const DESKTOP_WIDTH = 1315;
 
 tabs.forEach(tabsElement => {
   let panels = tabsElement.querySelectorAll('.tabs__panel');
   let tabButtons = tabsElement.querySelectorAll('.tabs__button');
   tabButtons.forEach((tabButton, i) => {
     tabButton.addEventListener('click', (evt) => {
+      if(window.innerWidth > DESKTOP_WIDTH) {
+        evt.preventDefault();
+      }
       tabButtons.forEach((element, j) => {
         if (element.classList.contains('tabs__button--active')) {
           element.classList.remove('tabs__button--active');
