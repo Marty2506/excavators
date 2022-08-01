efa327cd683115c50f8fd6069c767084050deb22
Marty <Marty2009@yandex.ru>
2022-08-01 23:05:37 +0300

    поправил часть ссылок и поменял порядок фильтров

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,7 +8,10 @@ pageHeaderBottom.classList.remove('page-header__bottom--no-js');
 menuSubs.forEach(submenu => {
   const link = submenu.parentNode.querySelector('.menu__link');
   if (link) {
-    link.addEventListener('click', () => {
+    link.addEventListener('click', (evt) => {
+      if (!link.classList.contains('menu__link--end')) {
+        evt.preventDefault();
+      }
       submenu.classList.add('menu__sub--active');
       const backButton = submenu.querySelector('.menu__back-button');
       backButton.addEventListener('click', () => {
